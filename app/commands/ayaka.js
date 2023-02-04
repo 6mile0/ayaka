16791a2826d54f37ca578370350721ad70ab2d4a
y0329 <kida_hirokazu@tuta.io>
2023-02-04 17:29:56 +0900

    add_mariadb
    [add]mariadbへの接続のためのコードを作成　ただし動かない可能性大

--- app/commands/ayaka.js
+++ app/commands/ayaka.js
@@ -3,30 +3,43 @@ import { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, But
 import { nanoid } from 'nanoid';
 import fs from 'node:fs';
 import { exec, execSync } from 'node:child_process';
-import Datastore from 'nedb';
-var db = new Datastore({
-    filename: '../db/data.db',
-    autoload: true
-});
-
+import mariadb from 'mariadb';
+const pool = mariadb.createPool({
+    host: "db",
+    user: "root",
+    password: "kidahirokazu",
+    database: "containers",
+    port: "3306"
+})
 function padZero(time) {
     return time.toString().padStart(2, "0")
 }
 
-const asighnPorts = async (userid) => {
+const asighnPorts = async () => {
     return new Promise((resolve, reject) => {
-        db.find({ userId: userid }, (err, docs) => {
-            if (err) {
-                reject(err);
-            } else {
-                console.log(docs);
-                if (docs.length == 0) {
-                    resolve(60000);
-                } else {
-                    resolve(docs[docs.length - 1].port + 1);
-                }
-            }
-        });
+        pool.getConnection()
+            .then(conn => {
+                conn.query("SELECT COUNT(*) FROM container_infos")
+                    .then((rows) => {
+                        var PortNum = Number(rows[0]["COUNT(*)"]);
+                        if (PortNum == 0) {
+                            resolve(60000);
+                        } else {
+                            resolve(60000 + PortNum);
+                        }
+                        console.log(60000 + PortNum);
+                        conn.end();
+                    })
+                    .catch(err => {
+                        //handle error
+                        console.log(err);
+                        conn.end();
+                    })
+
+            }).catch(err => {
+                //not connected
+                console.log(err);
+            });
     });
 }
 
@@ -40,7 +53,7 @@ const startUpAyaka = async (cfg) => {
             reject(err);
         }
 
-        exec(`docker run -d --name=${cfg.ctnId} -e PUID=${globalCfg.puId} -e PGID=${globalCfg.pgId} -e TZ=Asia/Tokyo -e PASSWORD=${cfg.pass} -e SUDO_PASSWORD=${cfg.sudoPass} -e DEFAULT_WORKSPACE=/config/workspace -p ${cfg.port}:8443 -v ${globalCfg.mntPoint}/${cfg.userId}/config:/config --restart unless-stopped ayaka/allpkg`, { timeout: 10000 },
+        exec(`docker run -d --name=${cfg.ctnId} -e PUID=${globalCfg.puId} -e PGID=${globalCfg.pgId} -e TZ=Asia/Tokyo -e PASSWORD=${cfg.pass} -e SUDO_PASSWORD=${cfg.sudoPass} -e DEFAULT_WORKSPACE=/config/workspace -p ${cfg.port}:8443 -v ${globalCfg.mntPoint}/${cfg.userId}/config:/config --restart unless-stopped ayaka/devspace`, { timeout: 10000 },
             function (error, stdout, stderr) {
                 // シェル上でコマンドを実行できなかった場合のエラー処理
                 if (error !== null) {
@@ -61,42 +74,50 @@ const startUpAyaka = async (cfg) => {
                 };
 
                 if (stdout.toString()) { // コンテナIDが一致した場合
-                    db.insert({
-                        ctnId: cfg.ctnId, // コンテナID
-                        userId: cfg.userId, // ユーザーID
-                        containerName: cfg.containerName, // コンテナ名
-                        password: cfg.pass, // パスワード
-                        sudoPassword: cfg.sudoPass, // sudoパスワード
-                        port: cfg.port, // 利用可能ポート
-                        startTime: new Date(), // 生成時間
-                        endTime: new Date().getTime() + 10800000, // 自動削除時間
-                    }, (err, newDoc) => {
-                        if (err) {
-                            reject(err);
-                        } else {
-                            resolve([
-                                `https://ayaka.cybroad.dev/attach/${newDoc.ctnId}`,
-                                newDoc.containerName,
-                                newDoc.password,
-                                newDoc.sudoPassword,
-                                `${padZero(newDoc.startTime.getFullYear())}/${padZero(newDoc.startTime.getMonth() + 1)}/${padZero(newDoc.startTime.getDate())} ${padZero(newDoc.startTime.getHours())}:${padZero(newDoc.startTime.getMinutes())}:${padZero(newDoc.startTime.getSeconds())}`, // 生成時間
-                                `${padZero(newDoc.startTime.getFullYear())}/${padZero(newDoc.startTime.getMonth() + 1)}/${padZero(newDoc.startTime.getDate())} ${padZero(newDoc.startTime.getHours() + 3)}:${padZero(newDoc.startTime.getMinutes())}:${padZero(newDoc.startTime.getSeconds())}`, // 3時間後
-                                newDoc.port,
-                                "mira"
-                            ]);
-                        }
-                    });
+                    pool.getConnection()
+                        .then(conn => {
+                            var date = new Date();
+                            var date = date.getUTCFullYear() + '-' +
+                                ('00' + (date.getUTCMonth() + 1)).slice(-2) + '-' +
+                                ('00' + date.getUTCDate()).slice(-2) + ' ' +
+                                ('00' + date.getUTCHours()).slice(-2) + ':' +
+                                ('00' + date.getUTCMinutes()).slice(-2) + ':' +
+                                ('00' + date.getUTCSeconds()).slice(-2);
+                            conn.query(`insert into container_infos values(${cfg.ctnId},${cfg.userId},${cfg.containerName},${cfg.pass},${cfg.sudoPass},${cfg.port},${date},${new Date().getTime() + 10800000})`)
+                                .then((rows) => {
+                                    console.log("sucesss");
+                                    resolve([
+                                        `https://ayaka.cybroad.dev/attach/${cfg.ctnId}`,
+                                        cfg.containerName,
+                                        cfg.password,
+                                        cfg.sudoPassword,
+                                        `${padZero(cfg.startTime.getFullYear())}/${padZero(cfg.startTime.getMonth() + 1)}/${padZero(cfg.startTime.getDate())} ${padZero(cfg.startTime.getHours())}:${padZero(cfg.startTime.getMinutes())}:${padZero(cfg.startTime.getSeconds())}`, // 生成時間
+                                        `${padZero(cfg.startTime.getFullYear())}/${padZero(cfg.startTime.getMonth() + 1)}/${padZero(cfg.startTime.getDate())} ${padZero(cfg.startTime.getHours() + 3)}:${padZero(cfg.startTime.getMinutes())}:${padZero(cfg.startTime.getSeconds())}`, // 3時間後
+                                        cfg.port,
+                                        "mira"
+                                    ]);
+                                    conn.end();
+                                })
+                                .catch(err => {
+                                    //handle error
+                                    console.log(err);
+                                    reject(err);
+                                    conn.end();
+                                })
 
-                } else {
-                    reject("コンテナの生成に失敗しました。");
+                        }).catch(err => {
+                            console.log(err);
+                            reject(err);
+                        })
                 }
-            });
+            }
+        )
     });
 }
 
 export default {
     data: new SlashCommandBuilder()
-        .setName('ayaka')
+        .setName('create')
         .setDescription('コンテナ型独立開発環境を立ち上げることができます。3hで自動停止しますが、延長をすることが可能です。'),
     async execute(interaction) {
         console.log(interaction.user.id + " がコマンドを実行しました");
@@ -109,6 +130,7 @@ export default {
             "sudoPass": nanoid(32), // sudoパスワード
             "port": ports, // 利用可能ポート
         };
+        console.log(containerInfo);
 
         await startUpAyaka(containerInfo).then((res) => {
             console.log(res);
