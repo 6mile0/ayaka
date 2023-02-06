cdadc70ac9910b0596dd908aeb6e82b4eb46e527
6mile <me@6mile.dev>
2023-02-06 18:50:55 +0900

    [Add] 関数分割

--- app/app.js
+++ app/app.js
@@ -1,11 +1,9 @@
 import globalCfg from "./config.json" assert { type: "json" }; // 設定ファイル読み込み
-import dbCfg from "./dbCredentials.json" assert { type: "json" };
 import { Client, GatewayIntentBits, Collection, EmbedBuilder } from 'discord.js';
 import fs from 'node:fs';
 import path from 'path';
-import { execSync } from 'node:child_process';
 import { fileURLToPath } from 'url';
-import mysql from 'mysql2';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const client = new Client({
@@ -25,90 +23,6 @@ for (const file of commandFiles) {
     }
 }
 
-async function getCtnId(userId) {
-    return new Promise((resolve, reject) => {
-        let db = mysql.createConnection(dbCfg); // データベースに接続
-        try {
-            db.execute(
-                `SELECT container_id FROM users WHERE user_id = ${userId}`,
-                function (err, res) {
-                    if (err) {
-                        console.log(err);
-                        reject(err);
-                    }
-                    console.log(res);
-                    if (!res) {
-                        reject("コンテナが見つかりません");
-                    } else {
-                        try {
-                            const ctnId = res[res.length - 1].container_id; // コンテナIDを取得(最新のもの)
-                            resolve(ctnId);
-                        } catch (e) {
-                            console.log(e);
-                            reject("コンテナが見つかりません");
-                        }
-                    }
-                }
-            );
-        } catch (err) {
-            console.log(err);
-            reject(err);
-        } finally {
-            db.end();
-        }
-    });
-}
-
-async function shitDownAyaka(ctnId) {
-    return new Promise((resolve, reject) => {
-        try {
-            var res = execSync(`docker exec ayaka-nginx-1 sh /etc/nginx/conf.d/remove.sh ${ctnId}`); // ポートを削除
-            if (res.toString().trim() == "Reloading nginx: nginx.") {
-                console.log(ctnId);
-                try {
-                    var result = execSync(`docker kill $(docker ps -a -q -f name=${ctnId})`);
-                    console.log(result.toString().trim());
-                    if (result.toString().trim() == "") {
-                        reject("削除失敗"); // コンテナ削除失敗
-                    } else {
-                        resolve(ctnId); // コンテナ削除成功
-                    }
-                } catch (e) {
-                    reject([e]); // 例外1
-                }
-            } else {
-                console.log(res);
-                reject(`${ctnId}の外部接続設定に失敗しました`);
-            }
-        } catch {
-            reject("外部接続設定の解除で例外が発生しました");
-        }
-    });
-}
-
-async function delRecord(ctnId) {
-    return new Promise((resolve, reject) => {
-        let db = mysql.createConnection(dbCfg); // データベースに接続
-        try {
-            db.execute(
-                `DELETE FROM users WHERE container_id = "${ctnId}"`,
-                function (err, res) {
-                    if (err) {
-                        console.log(err);
-                        reject(err);
-                    }
-                    console.log(res);
-                    resolve(ctnId); // コンテナ削除成功
-                }
-            );
-        } catch (err) {
-            console.log(err);
-            reject(err);
-        } finally {
-            db.end();
-        }
-    });
-}
 
 console.log(`${globalCfg.botName} ${globalCfg.ver} を起動します...`);
 client.once("ready", async () => {
--- app/commands/create.js
+++ app/commands/create.js
@@ -1,133 +1,8 @@
 import globalCfg from "../config.json" assert { type: "json" };
-import dbCfg from "../dbCredentials.json" assert { type: "json" };
+import { asighnPorts, makeUserFolder, startUpAyaka, addProxy, addRecord } from "../functions/create.js";
 import { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } from 'discord.js';
-import { execSync } from 'node:child_process';
 import { nanoid, customAlphabet } from 'nanoid';
-import fs from 'node:fs';
-import mysql from 'mysql2';
 const coustomNanoid = customAlphabet('1234567890abcdef', 8);
-
-async function addProxy(cfg) { // 利用可能ポートを割り当てる関数
-    return new Promise((resolve, reject) => {
-        try {
-            var res = execSync(`docker exec ayaka-nginx-1 sh /etc/nginx/conf.d/add.sh ${cfg.ctnId} ${cfg.port}`); // ポートを追加
-            if (res.toString().trim() == "Reloading nginx: nginx.") {
-                resolve(`${cfg.ctnId}の外部接続設定(${cfg.port}番)を追加しました`);
-            } else {
-                console.log(res.toString());
-                reject(`${cfg.ctnId}の外部接続設定に失敗しました`);
-            }
-        } catch {
-            reject(`${cfg.ctnId}の外部接続設定に失敗しました`);
-        }
-    });
-};
-
-async function asighnPorts() { // 利用可能ポートを割り当てる関数
-    return new Promise((resolve, reject) => {
-        try {
-            let db = mysql.createConnection(dbCfg); // データベースに接続
-            db.execute(
-                "SELECT * FROM users",
-                function (err, res) {
-                    if (err) {
-                        console.log(err);
-                        reject(err);
-                    }
-                    console.log(res);
-                }
-            );
-            db.execute(
-                "SELECT COUNT(*) AS count FROM users",
-                function (err, res) {
-                    if (err) {
-                        console.log(err);
-                        reject(err);
-                    }
-                    console.log(res[0].count);
-                    resolve(60000 + res[0].count);
-                }
-            );
-        } catch (err) {
-            console.log(err);
-            reject(err);
-        }
-    });
-}
-
-async function startUpAyaka(cfg, interaction) {
-    return new Promise((resolve, reject) => {
-        try {
-            if (!fs.existsSync(`${globalCfg.mntPoint}/${cfg.userId}/config`)) {
-                fs.mkdirSync(`${globalCfg.mntPoint}/${cfg.userId}/config`, { recursive: true }); // ユーザーフォルダを作成
-                console.log("ユーザーフォルダを作成しました");
-            }
-        } catch (err) {
-            reject(err);
-        }
-
-        try {
-            var res = execSync(`docker run -d --name=${cfg.ctnId} -e PUID=${globalCfg.puId} -e PGID=${globalCfg.pgId} -e TZ=Asia/Tokyo -e PASSWORD=${cfg.pass} -e SUDO_PASSWORD=${cfg.sudoPass} -e DEFAULT_WORKSPACE=/config/workspace -p ${cfg.port}:8443 -v ${globalCfg.mntPoint}/${cfg.userId}/config:/config --restart unless-stopped ayaka/allpkg`);
-            if (res.toString().trim() == "") {
-                reject("コンテナの起動に失敗しました");
-            } else {
-                console.log("コンテナを起動しました");
-            }
-
-            try {
-                var nowIimeMs = Math.round(new Date().getTime() / 1000); // 現在時刻(ミリ秒)
-                var createdTime = new Date(nowIimeMs * 1000); // 生成時間
-                var expiredTime = new Date((nowIimeMs + (3600 * 3)) * 1000); // 3時間後(自動削除予定時刻)
-
-                var intervalID = setInterval(() => {
-                    if (nowIimeMs >= expiredTime) { // 3時間後ミリ秒一致時
-                        const timerEmbed = new EmbedBuilder()
-                            .setColor("#ED4245")
-                            .setTitle('あなたのコンテナは停止されました')
-                            .setDescription("3時間経過し、延長申請がなかったためコンテナを削除しました。\nご利用ありがとうございました。")
-                            .addFields(
-                                { name: 'コンテナ名', value: cfg.containerName },
-                                { name: '作成者', value: interaction.user.username },
-                                { name: '作成日時', value: createdTime.toLocaleString('ja-JP') },
-                            )
-                            .setTimestamp()
-                        interaction.channel.send({ embeds: [timerEmbed] });
-                        setTimeout(() => {
-                            clearInterval(intervalID);
-                        });
-                    }
-                }, 1000);
-
-                let db = mysql.createConnection(dbCfg); // データベースに接続
-                db.execute(
-                    `INSERT INTO users VALUES ("${cfg.userId}","${cfg.ctnId}","${cfg.containerName}","${cfg.pass}","${cfg.sudoPass}",${cfg.port},${nowIimeMs},${nowIimeMs + 10800000},${intervalID},0)`,
-                    function (err, res) {
-                        if (err) {
-                            console.log(err);
-                            reject(err);
-                        }
-                        resolve([
-                            `${globalCfg.serviceDomain}/${cfg.ctnId}/login`, // アクセスURL
-                            cfg.containerName, // コンテナ名
-                            cfg.pass, // パスワード
-                            cfg.sudoPass, // sudoパスワード
-                            createdTime.toLocaleString('ja-JP'), // 生成時間
-                            expiredTime.toLocaleString('ja-JP'), // 削除予定時刻
-                            cfg.port, // ポート
-                            "mira" // 収容サーバ
-                        ]);
-                    }
-                );
-                db.end();
-            } catch (err) {
-                console.log(err);
-                reject(err);
-            }
-        } catch (err) {
-            reject(err);
-        }
-    });
-}
 
 export default {
     data: new SlashCommandBuilder()
@@ -135,61 +10,77 @@ export default {
         .setDescription('コンテナ型独立開発環境を立ち上げることができます。3hで自動停止しますが、延長をすることが可能です。'),
     async execute(interaction) {
         console.log(interaction.user);
+
         const ports = await asighnPorts(); // 利用可能ポートを取得
         const containerInfo = {
+            "serviceDomain": globalCfg.serviceDomain, // サービスドメイン
             "ctnId": nanoid(16), // コンテナID
             "userId": interaction.user.id, // ユーザーID
             "containerName": "ayaka-" + coustomNanoid(), // コンテナ名
             "pass": nanoid(20), // パスワード
             "sudoPass": nanoid(32), // sudoパスワード
             "port": ports, // 利用可能ポート
+            "nowIimeMs": 0,
+            "expiredMs": 0,
+            "createdTime": new Date(), // 生成時間
+            "expiredTime": new Date(), // 削除予定時刻
+            "intervalID": 0 // タイマーID
         };
 
-        await addProxy(containerInfo); // プロキシを追加
-        await startUpAyaka(containerInfo, interaction).then((res) => {
-            console.log(res);
-            const message = new EmbedBuilder()
-                .setColor(0x32cd32)
-                .setTitle('コンテナが準備できたよ！')
-                .setDescription("開発環境の生成が完了しました。以下の情報を確認してください。")
-                .addFields(
-                    { name: 'アクセスURL', value: res[0] },
-                    { name: 'コンテナ名', value: res[1] },
-                    { name: 'エディタログインパスワード', value: "`" + res[2] + "`" },
-                    { name: 'sudoパスワード', value: "`" + res[3] + "`" },
-                    { name: '生成日時', value: res[4] },
-                    { name: '自動削除予定日時', value: res[5] },
-                    { name: '利用可能ポート', value: String(res[6]) },
-                    { name: '収容サーバ', value: res[7] },
-                )
-                .setFooter({ text: `ayaka Ver ${globalCfg.ver} `, iconURL: globalCfg.icon });
+        Promise.all([makeUserFolder(containerInfo), startUpAyaka(containerInfo, interaction), addProxy(containerInfo), addRecord(containerInfo)]).then(async (res) => {
+            if (res[0] == 0 && res[1] == 0 && res[2] == 0 && res[3] == 0) {
+                console.log(res);
 
-            const controlBtn = new ActionRowBuilder()
-                .addComponents(
-                    new ButtonBuilder()
-                        .setCustomId('stop')
-                        .setLabel("コンテナ停止(削除)")
-                        .setStyle(ButtonStyle.Danger),
-                )
-                .addComponents(
-                    new ButtonBuilder()
-                        .setCustomId('extend')
-                        .setLabel("利用期間を延長する(3h)")
-                        .setStyle(ButtonStyle.Success)
-                        .setDisabled(true),
-                )
-                .addComponents(
-                    new ButtonBuilder()
-                        .setCustomId('export')
-                        .setLabel("データをエクスポートする")
-                        .setStyle(ButtonStyle.Primary)
-                        .setDisabled(true),
-                )
-            interaction.reply({
-                ephemeral: true, embeds: [message], components: [controlBtn]
-            });
-        }).catch((error) => {
-            console.log(error);
+                const message = new EmbedBuilder()
+                    .setColor(0x32cd32)
+                    .setTitle('コンテナが準備できたよ！')
+                    .setDescription("開発環境の生成が完了しました。以下の情報を確認してください。")
+                    .addFields(
+                        { name: 'アクセスURL', value: `${containerInfo.serviceDomain}+ ${containerInfo.ctnId}` },
+                        { name: 'コンテナ名', value: containerInfo.containerName },
+                        { name: 'エディタログインパスワード', value: "`" + containerInfo.pass + "`" },
+                        { name: 'sudoパスワード', value: "`" + containerInfo.sudoPass + "`" },
+                        { name: '生成日時', value: containerInfo.createdTime.toLocaleString('ja-JP') },
+                        { name: '自動削除予定日時', value: containerInfo.expiredTime.toLocaleString('ja-JP') },
+                        { name: '利用可能ポート', value: String(containerInfo.port) },
+                        { name: '収容サーバ', value: "JP-01" },
+                    )
+                    .setFooter({ text: `ayaka Ver ${globalCfg.ver} `, iconURL: globalCfg.icon });
+
+                const controlBtn = new ActionRowBuilder()
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setCustomId('stop')
+                            .setLabel("コンテナ停止(削除)")
+                            .setStyle(ButtonStyle.Danger),
+                    )
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setCustomId('extend')
+                            .setLabel("利用期間を延長する(3h)")
+                            .setStyle(ButtonStyle.Success)
+                            .setDisabled(true),
+                    )
+                    .addComponents(
+                        new ButtonBuilder()
+                            .setCustomId('export')
+                            .setLabel("データをエクスポートする")
+                            .setStyle(ButtonStyle.Primary)
+                            .setDisabled(true),
+                    )
+                interaction.reply({
+                    ephemeral: true, embeds: [message], components: [controlBtn]
+                });
+            } else {
+                const message = new EmbedBuilder()
+                    .setColor(0xFF0000)
+                    .setTitle('エラーが発生しました')
+                    .setDescription("[E1001] コンテナの起動に失敗しました。")
+                    .setFooter({ text: `ayaka Ver ${globalCfg.ver} `, iconURL: globalCfg.icon });
+                interaction.reply({ ephemeral: true, embeds: [message] });
+            }
+        }).catch((e) => {
+            console.log(e);
             const message = new EmbedBuilder()
                 .setColor(0xFF0000)
                 .setTitle('エラーが発生しました')
